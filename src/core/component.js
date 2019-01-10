566b60d316fea8134f7a32ba71cb2ade4a0b7737
karak <karak97@gmail.com>
2019-01-10 15:36:15 +0900

    fixed: typo

--- src/core/component.js
+++ src/core/component.js
@@ -11,7 +11,7 @@ const COMPONENT_CORE = Object.freeze({
   $$(selector){ return $$(selector, this.root) },
   mixin(name) {
     // extend this component with this mixin
-    Object.assing(this, MIXINS_MAP.get(name))
+    Object.assign(this, MIXINS_MAP.get(name))
   },
   // defined during the component creation
   css: null,
